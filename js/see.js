d1ed04465bdd16de4e53c75fc19d23131c567f5d
Sen <sl@sl088.com>
2013-04-26 17:20:02 +0000

    * 更近一些，支持了一些大小写切割，以及新标签方式

--- js/see.js
+++ js/see.js
@@ -1,24 +1,35 @@
 var currentRequest = null; //当前请求
+var site_url = "http://see.sl088.com";
 
-//获得最后一个字符，看起来还有更好的方式啊
-function get_lastchar(text) {
-	if (text.length > 0) {
-		return {
-			"last": text.charAt(text.length - 1),
-			"str": text.substr(0, text.length - 1)
-		}; //返回一个原型包含位置和长度
+/* 常规性配置 */
+var perfix_edit = "+"; //前缀编辑模式
+var perfix_edit_newtab = "++"; //前缀编辑模式、新窗口，它似乎依赖于前者
+
+/* 常规检查官-检查是否在特定的编辑模式下 
+ * 返回构建：
+ * isedit:是否编辑，isnew:是否新建，newtext:过滤后的文字
+ */
+function edit_chk(text){ //检查编辑模式
+		var result={isedit: false, isnew: false, newtext: text}; //返回构造
+
+	if (str_chklast(text,perfix_edit) || str_chklast(text,perfix_edit_newtab)) {
+		result.isedit = true; //设置标记
+		result.newtext = str_getlast(text,perfix_edit.length+1).str; //返回剩余的一部分
 	}
-	return null; //送回一个空，不介意吧
+	if(str_chklast(text,perfix_edit_newtab)) {
+			isedit = true; //单独的标记
+	}
+	return result;//返回构建
 }
 
-//大写首字母的，别的的不变
-function up_first_letter(text){
-	return text.charAt(0).toUpperCase()+text.substr(1,text.length);
-}
-
-//绑定输入事件
+/* 输入变动 
+ * 这是一切工作的核心
+ */
 chrome.omnibox.onInputChanged.addListener(function (text, suggest) {
-	var isedit = false; //编辑模式
+	var req_url; //申请url，json
+	var str_new_win="进入<url>当前海域</url>"; //新窗口的玩意？
+
+	var edit_type; //编辑模式的玩意
 
 	//停止上次事件，看起来像是做了这个
 	if (currentRequest != null) {
@@ -27,19 +38,23 @@ chrome.omnibox.onInputChanged.addListener(function (text, suggest) {
 		currentRequest = null;
 	}
 
-	put_info("进入森亮号航海见识开始探索[%s]");
+	put_info("进入森亮号航海见识开始探索[<match>%s</match>]");
 
-	if (text.length > 0 && text != "最近" ) { //过滤最近
+	if (text.length > 0 && text != "最近" ) { //过滤最近，但不排除无
 		//处理增加模式
-		if (get_lastchar(text).last == "+") {
-			isedit = true; //设置标记
-			//处置默认的玩意，默认的时候传出去的content就是本身咯
-			put_info("进入森亮号航海见识开始建造: " + get_lastchar(text).str);
-			//todo:如果原始有加号呢？
-			text = get_lastchar(text).str; //排除+号来工作
+		edit_type = edit_chk(text); //检查类型
+		text = edit_type.newtext; //文字也处理了
+		if (edit_type.isnew){
+			//新的标记方式，字符串全部索引起来？
+			str_new_win = "从<url>最近的海域</url>进入";
 		}
+		if (edit_type.isedit)
+		{
+			put_info("它还不存在,现在<url>建造</url>见识: <url>[" + str_getlast(text).str +"]</url>!"+str_new_win );
+		}
+		req_url = site_url + "/w/api.php?action=opensearch&suggest&search=" + text; //构造字串
 		//定义当前请求函数，以便后来请求
-		currentRequest = get_suggests(text, function (data, org_text) { //处理返回的json如何处置
+		currentRequest = get_json(req_url, function (data, org_text) { //处理返回的json如何处置
 			var results = [];
 			var result_arry = data[1]; //返回的数组，长度0就是没有结果
 			if (result_arry.length == 0) {
@@ -48,31 +63,56 @@ chrome.omnibox.onInputChanged.addListener(function (text, suggest) {
 			//这是每一个结果的处置
 			for (var index = 0; index < result_arry.length; index++) { //处理第一项
 				var data_take = result_arry[index]; //处理这个玩意
-				if (data_take == up_first_letter(org_text)) //完全匹配-除了首字母
+				if (data_take == str_up1letter(text)) //完全匹配-除了首字母
 				{
 					//一致提醒
-					if (isedit){
-							put_info("进入航海见识中立即开始<dim>编辑</dim>见识<url>[" + org_text + "]</url>！"); //处理不一致的文字
-					}else {put_info("噢!太好了!探索到存在<url>["+ org_text + "]</url>的见识!前往所在地吗?");}
+					if (edit_type.isedit){
+							put_info("探索到了!<url>重新</url	>见识<url>[" + text + "]</url>!"+str_new_win); //处理不一致的文字
+					}else {put_info("噢!太好了!探索到存在<url>["+ text + "]</url>的见识!前往所在地吗?");}
 
 				}
 				//制造高亮玩意
-				var re = new RegExp(org_text, "i");
-				var match_str=data_take.replace(re,"<match>"+org_text+"</match>"); //高亮有的话
+				var text_re = new RegExp(text, "i");
+				var match_part = data_take.match(text_re);
+				var match_str=data_take.replace(match_part,"<match>"+ match_part +"</match>"); //高亮有的话，被处理了大小写
 				//push入数据
 				results.push({
 					content: data_take, //这是发送给输入事件的数据，如果和输入一样，不会被送入
-					description: match_str + "\t       <dim>->存在于在航海见识 正在更深入探索</dim>" //这是描述
+					description: match_str + "\t       <dim>->存在于在航海见识,正在更深入探索</dim>" //这是描述
 				});
 			}
 			//玩意不知道是干嘛的-它是直接传递给回调函数处理结果的玩意
 			suggest(results); //提交结果，完事
 			//等待更深一步探索
-
+			//标题们
+			titles_all = "Mediawiki%20api%20%E9%87%8D%E5%AE%9A%E5%90%91%E6%A3%80%E6%9F%A5%7CMountain%20Lion%20%E5%AE%89%E8%A3%85%7C%E9%A6%96%E9%A1%B5"
+			req_url = site_url + "/w/api.php?action=query&prop=categories&format=json&cllimit=6&redirects&indexpageids&titles="+titles_all
+			//开始解析
+			currentRequest = get_json(req_url, function (data) {
+				if (data.query.pageids.length==0) //没有返回，几乎不会发生
+				{
+					return false;
+				}
+				var page_ids_arr=data.query.pageids
+				//处理数据
+				console.log(data);
+				for (index=0; index<page_ids_arr.length ; index++ )
+				{
+					//处理得到的标题们，会全部得到吗
+					//保存到一个临时表
+					//data.query.titles[data.query.pageids]
+				}
+				//再次push就好
+			})
+		
+		//结束处理搜索
 		});
 	} else { //直接现实最近的
 		put_info("输入标题来探索航海见识,而这是<url>[最近]</url>见识：");
-		currentRequest = get_recently(text, function (data) {
+		//todo，函数式改写，太有点混世了
+		//仅获得六个
+		req_url = site_url + "/w/api.php?action=query&list=recentchanges&format=json&rcnamespace=0&rclimit=6&rctype=edit%7Cnew&rctoponly";
+		currentRequest = get_json(req_url, function (data) {
 			var results = [];
 			//todo：不要一次性算出两级，错误太多
 			var result_arry = data.query.recentchanges; //返回的数组，长度0就是没有结果
@@ -95,7 +135,6 @@ chrome.omnibox.onInputChanged.addListener(function (text, suggest) {
 });
 
 //恢复到默认玩意，没有任何提醒
-
 function resetDefaultSuggestion() {
 	chrome.omnibox.setDefaultSuggestion({
 		description: ' '
@@ -129,10 +168,8 @@ function put_error(e) {
 	put_info("探索见识的时候发生了错误: [" + e.message + "]");
 }
 
-// 获得搜索结果
-
-function get_suggests(query, callback) {
-	var req_url = "http://see.sl088.com/w/api.php?action=opensearch&suggest&search=" + query; //构造字串
+// 获得json搜索结果
+function get_json(req_url, callback) {
 	var req = new XMLHttpRequest();
 
 	req.open("GET", req_url, true);
@@ -140,7 +177,7 @@ function get_suggests(query, callback) {
 		if (this.status == 200) {
 			try {
 				//传递返回内容
-				callback(JSON.parse(this.responseText), query); //传回原始文字，这里过滤了reponseText
+				callback(JSON.parse(this.responseText)); //传回原始信息，这里过滤了reponseText
 			} catch (e) {
 				put_error(e);
 			}
@@ -152,52 +189,51 @@ function get_suggests(query, callback) {
 	return req; //返回原型
 }
 
-// 获得最近结果
-
-function get_recently(query, callback) {
-	//传输地址
-	var req_url = "http://see.sl088.com/w/api.php?action=query&list=recentchanges&format=json&rcnamespace=0&rclimit=10&rctype=edit%7Cnew&rctoponly";
-	var req = new XMLHttpRequest();
-
-	req.open("GET", req_url, true);
-	req.onload = function () {
-		if (this.status == 200) {
-			try {
-				//传递返回内容
-				callback(JSON.parse(this.responseText));
-			} catch (e) {
-				put_error(e);
-			}
-		} else {
-			put_error(e);
-		}
-	};
-	req.send();
-	return req; //返回原型
-}
-
-//前行到网站
-
-function navigate(url) {
-	chrome.tabs.getSelected(null, function (tab) {
+/* 前往海域-当前海域
+ */
+function tab_go(url) {
+	chrome.tabs.getSelected(null, function (tab) { //这就是获得了当前的tab哦
 		chrome.tabs.update(tab.id, {
 			url: url
 		});
 	});
 }
 
-// 选择了一项见识里的玩意
+/* 前往海域-附近海域 
+ */
+function tab_new(url) {
+		chrome.tabs.getSelected(null, function (tab) { //这就是获得了当前的tab哦
+		chrome.tabs.create({
+			index: tab.index + 1, //序号+1
+			url: url
+		});
+	});
+}
+
+/* 当：选择了一项见识里的玩意
+ * 将：去往那个见识的地方
+ */
 chrome.omnibox.onInputEntered.addListener(function (text) {
-	if (get_lastchar(text).last == "+")
-	//新增加一个玩意
-		navigate("http://see.sl088.com/w/index.php?action=edit&title=" + get_lastchar(text).str);
-	else
+	tips_title = "森亮Chrome扩展 森亮见识墨水/探索提醒"
+	var edit_link = "http://see.sl088.com/w/index.php?action=edit&editintro="+ 
+					encodeURIComponent(tips_title) + "&title=" 
+	if (str_chklast(text,perfix_edit_newtab)){ //一起+那就放回去
+		//新增加一个玩意
+		tab_new(edit_link+str_getlast(text,2).str); //末尾多算一点
+
+	}else if (str_chklast(text,perfix_edit)){
+		tab_go(edit_link+str_getlast(text).str);
+
+	}else{
 	//正常情况下，当一样的时候让它自己跳转
-		navigate("http://see.sl088.com/w/index.php?search=" + text);
+		tab_go("http://see.sl088.com/w/index.php?search=" + text);
+	}
 });
 
-//获得当前的tab，留待使用
-Get_Currtab = function () {
+/* 获得当前的tab，留待使用
+ * 它可以获得，而且看起来并不是很疯狂
+ */
+tab_getnow = function () {
 	chrome.tabs.getSelected(function (tab) {
 		console.debug("当前的标签是:", tab); //tab.url 就是url地址了
 	});
--- js/str_unit.js
+++ js/str_unit.js
@@ -1,17 +1,42 @@
-/* 字符串子函数们 */
+/* 字符串处理的子函数们，它们为字符串而生 */
 
-//获得最后一个字符，看起来还有更好的方式啊
-function get_lastchar(text) {
-	if (text.length > 0) {
-		return {
-			"last": text.charAt(text.length - 1),
-			"str": text.substr(0, text.length - 1)
-		}; //返回一个原型包含位置和长度
+/* todo,匹配一个尾巴是否一致的函数
+
+/* 匹配尾部字符是否一致，并进行切割
+ * 送入原始字符，匹配字符
+ * 返回: 布尔值，判决结果
+*/
+function str_chklast(text,last){
+	//如果是正则表达式方式，从尾部匹配开始，计算是否有效匹配获得
+	if (last.length > 0) //有传入
+	{
+		return (str_getlast(text,last.length).last==last) //匹配送回去有效
 	}
-	return null; //送回一个空，不介意吧
+	return false;
 }
 
-//大写首字母的，别的的不变
-function up_first_letter(text){
+/* 获得最后的字符，如果没有长度就获得最后一个
+ * 返回：
+ * result.last: 最后字符
+ * result.str: 切除尾巴后的字符
+ */
+function str_getlast(text,how_long) {
+	if (typeof(how_long) == "undefined")
+	{
+		how_long=1; //未指定赋予1
+	}
+	if (text.length > 0) {
+		return {
+			"last": text.substr(text.length - how_long, how_long), //切断尾部
+			"str": text.substr(0, text.length - how_long)
+		}; //返回一个原型包含位置和长度
+	}
+	return null; //送回一个空，不介意吧，不算太好的主意
+}
+
+/* 大写首字母的，别的的不变
+ * 返回带大写的首字母
+ */
+function str_up1letter(text){
 	return text.charAt(0).toUpperCase()+text.substr(1,text.length);
 }
