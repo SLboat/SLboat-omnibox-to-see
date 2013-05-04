35f35d0d605bd62cf0bf42ea3ede89faa5728cb5
Sen <sl@sl088.com>
2013-05-04 05:46:59 +0000

    * 小约子字体库的加入

---
+++ script/fonts_fix.js
@@ -0,0 +1,126 @@
+/* Const - Font
+ * 这里储存字体相关的信息
+ * 等宽体直接返回真实长度就行
+ * 雅黑的话一个字是19 一个空格是6
+ * char_px_yh是雅黑的字库
+ * char_px_th是tahoma的字库
+ * 无法照顾到所有的字体 - 这很恐怖
+ * 但是还是疯狂的去做了
+ */
+
+function Fonts_fix(type){
+	var this.font_tp = type;  //字体信息 0是等宽 1是雅黑 2是tahoma
+}
+
+/* 常量值 */
+Fonts_fix.prototype.char_px_yh={A:13,B:12,C:13,D:14,E:10,F:10,G:14,H:15,I:6,J:8,K:12,L:10,M:19,N:15,O:15,P:12,Q:15,R:12,S:11,T:11,U:14,
+						V:13,W:19,X:12,Y:11,Z:12,a:11,b:12,c:10,d:12,e:11,f:7,g:12,h:12,i:5,j:7,k:10,l:5,m:18,n:12,o:12,p:12,q:12,r:6,s:9,t:7,u:12,v:10,w:15,x:10,
+						y:10,z:9,0:11,1:11,2:11,3:11,4:11,5:11,6:11,7:11,8:11,9:11,
+	
+}
+
+ //哇哦有够长的 - 还有一份
+Fonts_fix.prototype.char_px_th={A:10,B:10,C:10,D:12,E:10,F:9,G:12,H:12,I:6,J:7,K:10,L:8,M:14,N:12,O:12,P:9,Q:12,R:11,S:10,T:10,U:11,
+						V:10,W:16,X:10,Y:10,Z:10,a:9,b:10,c:8,d:10,e:9,f:5,g:10,h:10,i:3,j:5,k:8,l:3,m:15,n:10,o:9,p:10,q:10,r:6,s:7,t:5,u:10,v:8,w:13,x:8,
+						y:8,z:7,0:9,1:9,2:9,3:9,4:9,5:9,6:9,7:9,8:9,9:9}
+
+char_px_yh[' ']=6; //也许我应该这么稍稍处理下
+char_px_yh['@']=20;
+char_px_yh['!']=6;
+char_px_yh['?']=9;
+char_px_yh['+']=14;
+char_px_yh['=']=14;
+char_px_yh['-']=8;
+char_px_yh['#']=12;
+char_px_yh['$']=11;
+char_px_yh['%']=17;
+char_px_yh['^']=14;
+char_px_yh['&']=17;
+char_px_yh['*']=9;
+char_px_yh['\\']=8;
+char_px_yh['/']=8;
+char_px_yh['`']=6;
+char_px_yh['.']=5;
+char_px_yh['<']=14;
+char_px_yh['>']=14;
+char_px_yh[':']=5;
+char_px_yh[';']=5;
+char_px_yh['_']=9;
+char_px_yh['\'']=5;
+char_px_yh['"']=8;
+char_px_yh['~']=14;
+char_px_yh['[']=6;
+char_px_yh[']']=6;
+char_px_yh['|']=5;
+char_px_yh['{']=6;
+char_px_yh['}']=6; //字符处理完毕 - 虽然没啥用?
+
+
+char_px_th[' ']=5; //Tahoma的字符
+char_px_th['@']=16;
+char_px_th['!']=5;
+char_px_th['?']=8;
+char_px_th['+']=13;
+char_px_th['=']=13;
+char_px_th['-']=6;
+char_px_th['#']=13;
+char_px_th['$']=9;
+char_px_th['%']=18;
+char_px_th['^']=13;
+char_px_th['&']=12;
+char_px_th['*']=9;
+char_px_th['\\']=6;
+char_px_th['/']=6;
+char_px_th['`']=9;
+char_px_th['.']=5;
+char_px_th['<']=13;
+char_px_th['>']=13;
+char_px_th[':']=6;
+char_px_th[';']=6;
+char_px_th['_']=10;
+char_px_th['\'']=3;
+char_px_th['"']=7;
+char_px_th['~']=13;
+char_px_th['[']=6;
+char_px_th[']']=6;
+char_px_th['|']=6;
+char_px_th['{']=8;
+char_px_th['}']=8; //终于结束了 - 更多的字体伤不起
+
+/* 计算出字符串的实际长度
+ * 应用字体后的长度
+ * 返回的值是大致相当于多少空格
+ */
+function Fonts_fix.prototype={
+	len=function (str){
+	str = str.replace(/<\/?match>/g,"");
+	var len = 0;
+	if(font_tp==1){
+		for(var i = 0; i < str.length; i++){
+			len += char_px_yh[str[i]] || 19;  //是的 - 雅黑汉字19px 空格6px 要的只是最终的一个倍数
+		}
+		len = Math.floor(len % 6 > 3 ? len/6 +1 : len/6);  //意外的简单
+	}else if(font_tp==2){
+		for(var i = 0; i < str.length; i++){
+			len += char_px_th[str[i]] || 17;
+		}
+		len = Math.floor(len % 5 > 2 ? len/5 +1 : len/5);  //唔 另外的一个数据 大体结构已经有了 - 不需要改变太多
+	}else{
+		len = str.match(/[^ -~]/g) == null ? str.length : str.length + str.match(/[^ -~]/g).length;  //等宽字体?
+	}
+	return len;
+}
+
+/* 把字符串加上空格
+ * 是的看起来很困难 但是完成了
+ * 返回这个字符串
+ */
+fix=function (str, blanks){
+	var len = str_getfixedlen(str);
+	for(var i=0; i < blanks - len; i++){
+		str += " ";  //是的很笨 - 但是我不知道怎么做
+	}
+	return str;
+}
+
+} //方法封装完毕
